a41a33e3967fddd5e67e222ffc8a8337d29e5067
halych.ivan <galych.ivan@ukr.net>
2024-01-11 10:05:03 +0200

    add

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -2,15 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Config;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        $config = Config::find(1);
-        return view('__shop.layout', compact('config'));
+
+        $products = Product::inRandomOrder()->limit(6)->get();
+
+
+
+
+        $data = '';
+
+        return view('__shop.index', compact('products'));
+
+//        $config = Config::find(1);
+//        return view('__shop.layout', compact('config'));
     }
 
 }
---
+++ resources/views/__shop/index.blade.php
@@ -0,0 +1,177 @@
+@extends('__shop.layout')
+
+@section('title', 'Головна сторінка')
+
+
+@section('content')
+
+{{--    {{ dd( $products[1]->images[1]->image_path) }}--}}
+
+<!-- SLIDER AREA START -->
+<section class="slider-area-main">
+    <!-- slider -->
+    <div class="slider-area">
+        <div class="bend niceties preview-1">
+            <div id="ensign-nivoslider-3" class="slides">
+                <img src="img/home-1/slider-1.jpg" alt="" title="#slider-direction-1"  />
+                <img src="img/home-1/slider-2.jpg" alt="" title="#slider-direction-2"  />
+                <img src="img/home-1/slider-3.jpg" alt="" title="#slider-direction-3"  />
+            </div>
+            <!-- direction 1 -->
+            <div id="slider-direction-1" class="t-cn slider-direction">
+                <div class="slider-content t-lfl s-tb slider-1">
+                    <div class="title-container s-tb-c">
+                        <h1 class="title1">Best Collection</h1>
+                        <p class="title1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                        <h3 class="title3"><a href="#">SHOP NOW</a></h3>
+                    </div>
+                </div>
+            </div>
+            <!-- direction 2 -->
+            <div id="slider-direction-2" class="slider-direction">
+                <div class="slider-content t-cn s-tb slider-2">
+                    <div class="title-container s-tb-c">
+                        <h1 class="title1">Our gorgeous shop</h1>
+                        <h3 class="title3">for fashion</h3>
+                    </div>
+                </div>
+            </div>
+            <!-- direction 3 -->
+            <div id="slider-direction-3" class="slider-direction">
+                <div class="slider-content t-lfr s-tb slider-3">
+                    <div class="title-container s-tb-c">
+                        <h1 class="title1">Little Princes Shoe</h1>
+                        <h3 class="title3">Fashion Shoe</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- slider end-->
+</section>
+<!-- SLIDER AREA END -->
+
+
+
+    <!-- BANNER AREA START -->
+    <section class="banner-area">
+        <div class="container">
+            <div class="row">
+
+                            <div class="col-md-3 col-sm-3 xs-res-mrbtm">
+                                <div class="banner-left">
+                                    <a class="promo-link" href="{{route('category', $products[0]->category)}}">
+                                        <img src="{{ asset($products[0]->images[1]->image_path) }}" alt="" />
+                                        <h1>{{ $products[0]->category->title }}</h1>
+                                        <span class="promo-hover"></span>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-md-3 col-sm-3">
+                                <div class="banner-left-side">
+                                    <a class="mr-btm promo-link" href="{{route('product', $products[1])}}">
+                                        <img src="{{ asset($products[1]->image) }}" alt="" />
+                                        <h1>{{$products[1]->title}}</h1>
+{{--                                        <span class="sl-btn">SALE</span>--}}
+                                        <div class="promo-hover"></div>
+                                    </a>
+                                    <a class="promo-link xs-res-mrbtm" href="{{route('product', $products[2])}}">
+                                        <img src="{{ asset($products[2]->image) }}" alt="" />
+                                        <h1>{{$products[2]->title}}</h1>
+{{--                                        <span class="sl-btn">SALE</span>--}}
+                                        <div class="promo-hover"></div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-md-3 col-sm-3 xs-res-mrbtm">
+                                <div class="banner-right">
+                                    <a class="promo-link" href="{{route('category', $products[3]->category)}}">
+                                        <img src="{{ asset($products[3]->images[1]->image_path) }}" alt="" />
+                                        <h1>{{ $products[3]->category->title }}</h1>
+                                        <span class="promo-hover"></span>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-md-3 col-sm-3">
+                                <div class="banner-right-side">
+                                    <a class="mr-btm promo-link" href="{{route('product', $products[4])}}">
+                                        <img src="{{ asset($products[4]->image) }}" alt="" />
+                                        <h1>{{$products[4]->title}}</h1>
+            {{--                            <span class="sl-btn">SALE</span>--}}
+                                        <div class="promo-hover"></div>
+                                    </a>
+                                    <a class="promo-link" href="{{route('product', $products[5])}}">
+                                        <img src="{{ asset($products[5]->image) }}" alt="" />
+                                        <h1>{{$products[5]->title}}</h1>
+            {{--                            <span class="sl-btn">SALE</span>--}}
+                                        <div class="promo-hover"></div>
+                                    </a>
+                                </div>
+                            </div>
+
+            </div>
+        </div>
+    </section>
+
+<!-- BANNER AREA START -->
+<section class="banner-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-sm-3 xs-res-mrbtm">
+                <div class="banner-left">
+                    <a class="promo-link" href="#">
+                        <img src="img/home-1/banner-1.jpg" alt="" />
+                        <h1>MEN’S COLLECTION</h1>
+                        <span class="promo-hover"></span>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-3">
+                <div class="banner-left-side">
+                    <a class="mr-btm promo-link" href="#">
+                        <img src="img/home-1/banner-2.jpg" alt="" />
+                        <h1>NEW COLLECTION</h1>
+                        <span class="sl-btn">SALE</span>
+                        <div class="promo-hover"></div>
+                    </a>
+                    <a class="promo-link xs-res-mrbtm" href="#">
+                        <img src="img/home-1/banner-3.jpg" alt="" />
+                        <h1>BEST MEN’S COLLECTION</h1>
+                        <span class="sl-btn">SALE</span>
+                        <div class="promo-hover"></div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-3 xs-res-mrbtm">
+                <div class="banner-right">
+                    <a class="promo-link" href="#">
+                        <img src="img/home-1/banner-4.jpg" alt="" />
+                        <h1>WOMEN’S COLLECTION</h1>
+                        <span class="promo-hover"></span>
+                    </a>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-3">
+                <div class="banner-right-side">
+                    <a class="mr-btm promo-link" href="#">
+                        <img src="img/home-1/banner-5.jpg" alt="" />
+                        <h1>NEW COLLECTION</h1>
+                        <span class="sl-btn">SALE</span>
+                        <div class="promo-hover"></div>
+                    </a>
+                    <a class="promo-link" href="#">
+                        <img src="img/home-1/banner-6.jpg" alt="" />
+                        <h1>WOMEN’S COLLECTION</h1>
+                        <span class="sl-btn">SALE</span>
+                        <div class="promo-hover"></div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- BANNER AREA END -->
+
+
+
+@endsection
